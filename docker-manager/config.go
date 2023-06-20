e7ef69d963f87bc3115ec3378f45595d1d8b6bf4
onuragtas <agtasonur@gmail.com>
2023-06-21 00:42:35 +0300

    81 xdebug changed

--- docker-manager/config.go
+++ docker-manager/config.go
@@ -8,3 +8,12 @@ xdebug.remote_handler = dbgp
 xdebug.remote_mode = req
 xdebug.remote_host=%s
 xdebug.remote_port=%d`
+
+var xdebugConf8 = `
+zend_extension=xdebug.so
+xdebug.mode=debug
+xdebug.client_host=%s
+xdebug.start_with_request=yes
+xdebug.client_port=%d
+xdebug.discover_client_host=true
+`
