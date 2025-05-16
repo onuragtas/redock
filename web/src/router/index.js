14cc71205e4631032e69b4aeac45486ac7207ea6
onuragtas <agtasonur@gmail.com>
2025-05-16 14:14:33 +0300

    chore: update version to 1.0.168 and remove welcome route from router

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -3,14 +3,6 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
   {
-    meta: {
-      title: 'Welcome'
-    },
-    path: '/',
-    name: 'welcome',
-    component: () => import('@/views/LandingView.vue')
-  },
-  {
     // Document title tag
     // We combine it with defaultDocumentTitle set in `src/main.js` on router.afterEach hook
     meta: {
@@ -144,7 +136,7 @@ const routes = [
     meta: {
       title: 'Login'
     },
-    path: '/login',
+    path: '/',
     name: 'login',
     component: () => import('@/views/LoginView.vue')
   },
@@ -162,7 +154,7 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
-    return savedPosition || { top: 0 }
+    return savedPosition || {top: 0}
   }
 })
 
